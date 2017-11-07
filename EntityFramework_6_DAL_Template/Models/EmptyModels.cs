447c8ce8bda7c6625550f5b21b7b5d49cab03089
Jo <geoctr@yahoo.com>
2017-11-07 18:18:57 -0500

    clean

--- EntityFramework_6_DAL_Template/Models/EmptyModels.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace EntityFramework_6_DAL_Template.Models
-{
-    public static class EmptyModels
-    {
-        public static Department EmptyDepartment = new Department();
-        public static Employee EmptyEmployee     = new Employee();
-    }
-}
